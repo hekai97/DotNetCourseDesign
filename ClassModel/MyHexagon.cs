33f18cde03efc7dd8bd34f9f839da48223b4c6bb
hekai <1731673423@qq.com>
2022-05-25 23:37:10 +0800

    新增了六边形的类 新增了Hprose包 写了用户相似度的识别逻辑

---
+++ ClassModel/MyHexagon.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hprose.RPC;
+
+namespace ClassModel
+{
+    public class MyHexagon
+    {
+        Client client;
+        struct Point
+        {
+            public float x;
+            public float y;
+        }
+        public MyHexagon(List<float> hexagonPoint)
+        {
+            client=new Client("http://127.0.0.1:8082");
+            //TODO  从用户的六部图中把六边形的顶点算出来
+        }
+        Point[] point = new Point[6];
+        public float GetArea()
+        {
+            float area = client.Invoke<float>("getArea",new object[] {point});
+            return area;
+        }
+        public float GetOverloapArea(MyHexagon anotherHexagon)
+        {
+            bool isOverloap= client.Invoke<bool>("isOverloap", new object[] { this.point,anotherHexagon.point });
+            if (!isOverloap)
+            {
+                return 0.0f;
+            }
+            else
+            {
+                float overloapArea = client.Invoke<float>("getOverloapArea", new object[] { this.point, anotherHexagon.point });
+                return overloapArea;
+            }
+        }
+    }
+}
